99d5718e861087aae968fd08f869d6f292a54825
Bosklopper <eelco.bosklopper@capgemini.com>
2019-12-30 16:22:58 +0100

    gatsby starter cleanup

---
+++ src/pages/koekjes.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Koekjes = () => (
+  <Layout>
+    <SEO title="Koekjes" />
+    <h1>Geheime koekjes pagina!</h1>
+  </Layout>
+)
+
+export default Koekjes
