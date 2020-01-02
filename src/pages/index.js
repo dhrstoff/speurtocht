e159f9829dce49e4ad186b0956efc457e0d3aa61
Bosklopper <eelco.bosklopper@capgemini.com>
2020-01-02 18:25:37 +0100

    inputfield met url redirect

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,28 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState } from "react"
+import { Link, navigate } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>There there!</h1>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+
+const IndexPage = () => {
+  const [input, setInput] = useState(''); // '' is the initial state value
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div><input value={input} onInput={e => setInput(e.target.value)}/>
+      <button onClick={() => { magic(input) }}>Woosh</button></div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
+
+function magic(url) {
+  if (url === 'koekjes') {
+    navigate(url);
+  } else {
+    alert(`${url} is niet de spreuk die je zoekt`);
+  }
+}
 
 export default IndexPage
