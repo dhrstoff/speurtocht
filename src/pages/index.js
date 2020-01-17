371b4a0e2d40951655e649eb4ec7f7078e718d5d
Eelco Bosklopper <eelco.bosklopper@capgemini.com>
2020-01-17 22:02:01 +0100

    added wand icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,10 @@ const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
+      <h2>Welkom!</h2>
+      <p></p>
+      <p></p>
+      <p></p>
       <Link to="/page-2/">Go to page 2</Link>
     </Layout>
   )
