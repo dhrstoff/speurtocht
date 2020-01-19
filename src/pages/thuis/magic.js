a71b2223f2b427cab1fbe97f51f52bda440d3b85
Bosklopper <eelco.bosklopper@capgemini.com>
2020-01-19 10:26:44 +0100

    simplified and fixed decode puzzle

--- src/pages/thuis/magic.js
+++ src/pages/thuis/magic.js
@@ -23,8 +23,7 @@ const inputOptions = [
     url: "thuis/nat"
   }
 ]
-// Elke letter is met een vast aantal omhoog verplaatst in het alphabet afhankelijk van de volgorde. Nummer 1 is met 1 plek verplaatst. Letter 1 hiervan is nog 1 plek verplaatst
-//
+
 
 const Magic = () => {
   return (
@@ -34,10 +33,12 @@ const Magic = () => {
         The book flips open and lands on this page...
       </h2>
       <p>In the margins you see four strange words written from top to bottom..</p>
-      <p>eire</p>
-      <p>uelb</p>
-      <p>bxlqe</p>
-      <p>swpwn</p>
+      <ol>
+        <li>ehqd</li>
+        <li>uglb</li>
+        <li>bxoqe</li>
+        <li>swpan</li>
+      </ol>
       <MagicInput spells={inputOptions} />
     </Layout>
   )
