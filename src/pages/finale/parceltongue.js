32c9c9770fb38f9d6755f0171a3a89ccdf108ded
Bosklopper <eelco.bosklopper@capgemini.com>
2020-02-13 18:09:50 +0100

    Added glyph images

--- src/pages/finale/parceltongue.js
+++ src/pages/finale/parceltongue.js
@@ -1,12 +1,37 @@
 import React from "react"
-
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
+
+import img5 from "../../images/finale/5.jpg"
+import img6 from "../../images/finale/6.jpg"
+import img7 from "../../images/finale/7.jpg"
+import img8 from "../../images/finale/8.jpg"
 
 const Parceltongue = () => (
   <Layout>
     <SEO title="Parceltongue" />
-    {/* TODO add glyph text */}
+    <p>
+      I sure hope everything is going swimmingly and the rain isn't bringing you
+      down too much. Anyway, I present you with some gibberish!
+    </p>
+    <div style={{
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "column"
+    }}>
+      <p>
+        <img src={img5} alt="img5" />
+      </p>
+      <p>
+        <img src={img6} alt="img6" />
+      </p>
+      <p>
+        <img src={img7} alt="img7" />
+      </p>
+      <p>
+        <img src={img8} alt="img8" />
+      </p>
+    </div>
   </Layout>
 )
 
--- src/pages/finale/stories.js
+++ src/pages/finale/stories.js
@@ -4,19 +4,34 @@ import Layout from "../../components/layout"
 import SEO from "../../components/seo"
 import MagicInput from "../../components/magic-input"
 
+import img1 from "../../images/finale/1.jpg"
+import img2 from "../../images/finale/2.jpg"
+import img3 from "../../images/finale/3.jpg"
+import img4 from "../../images/finale/4.jpg"
+
 const Stories = () => (
   <Layout>
     <SEO title="Stories" />
     <h1>Darn walls of text!</h1>
     <p>Much cleaner like this:</p>
-    {/* TODO: glyph afbeeldingen
-      stenenkast
-      balkon
-      hobbit
-      hoed
-    */}
-
-    <MagicInput />
+    <div style={{
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "column"
+    }}>
+      <p>
+        <img src={img1} alt="img1" />
+      </p>
+      <p>
+        <img src={img2} alt="img2" />
+      </p>
+      <p>
+        <img src={img3} alt="img3" />
+      </p>
+      <p>
+        <img src={img4} alt="img4" />
+      </p>
+    </div>
   </Layout>
 )
 
