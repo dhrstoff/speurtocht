0a337950de2317e32fb969fe05eaa437fd32ed9f
Bosklopper <eelco.bosklopper@capgemini.com>
2020-02-13 17:51:58 +0100

    added pigpen image to cauldron

--- src/pages/brug/cauldron.js
+++ src/pages/brug/cauldron.js
@@ -1,6 +1,7 @@
 import React from "react"
-import Img from "gatsby-image"
-import { graphql } from "gatsby"
+// import Img from "gatsby-image"
+// import { graphql } from "gatsby"
+import image from "../../images/pigpen.svg"
 
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
@@ -8,6 +9,14 @@ import SEO from "../../components/seo"
 const Cauldron = ({ data }) => (
   <Layout>
     <SEO title="Cauldron" />
+    <img
+      src={image}
+      style={{
+        margin: "0 auto",
+        width: "500px",
+        display: "block",
+      }}
+    />
     {/* TODO: add pigpen image */}
     {/* <Img fluid={data.fileName.childImageSharp.fluid} alt="" /> */}
   </Layout>
