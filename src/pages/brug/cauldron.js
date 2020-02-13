b3d26b5b61aa0ede2f695d2e285388c703e77584
Bosklopper <eelco.bosklopper@capgemini.com>
2020-02-13 17:52:08 +0100

    pigpen

--- src/pages/brug/cauldron.js
+++ src/pages/brug/cauldron.js
@@ -1,6 +1,4 @@
 import React from "react"
-// import Img from "gatsby-image"
-// import { graphql } from "gatsby"
 import image from "../../images/pigpen.svg"
 
 import Layout from "../../components/layout"
@@ -17,21 +15,7 @@ const Cauldron = ({ data }) => (
         display: "block",
       }}
     />
-    {/* TODO: add pigpen image */}
-    {/* <Img fluid={data.fileName.childImageSharp.fluid} alt="" /> */}
   </Layout>
 )
-
-// export const query = graphql`
-//   query {
-//     fileName: file(relativePath: { eq: "morse.png" }) {
-//       childImageSharp {
-//         fluid(maxWidth: 500) {
-//           ...GatsbyImageSharpFluid
-//         }
-//       }
-//     }
-//   }
-// `
 
 export default Cauldron
