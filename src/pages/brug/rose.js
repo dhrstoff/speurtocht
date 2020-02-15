752e038a1125ffe016df2a913b9871902e0477cd
Eelco Bosklopper <eelco.bosklopper@capgemini.com>
2020-02-15 09:58:45 +0100

    parsnips to rose

--- src/pages/brug/rose.js
+++ src/pages/brug/rose.js
@@ -3,12 +3,17 @@ import React from "react"
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
 
+import MagicInput from "../../components/magic-input"
+
+
 const Rose = () => (
   <Layout>
     <SEO title="Rose" />
     <h1>Rose</h1>
     <p>Awfully cleverly done! I'm sure you ladies are being the definition of inconspicuousness in this beautiful muggle world!</p>
+    <p>But fine words butters no parsnips, so let's move on!</p>
     <p>The six of you (or is it more?) should have zero trouble finding the next location you should go two!</p>
+    <MagicInput />
   </Layout>
 )
 
